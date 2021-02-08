7d4b058fd176bdd1fefb11ce6005a3870cddecf9
Leonard <Leonard@LAPTOP-GP1NC36J>
2021-02-08 22:02:35 +0530

    added class results declared variable x

--- src/gitdemo/student.java
+++ src/gitdemo/student.java
@@ -1,10 +1,14 @@
 package gitdemo;
-
+class results
+{
+	
+}
 public class student {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		
 	}
 
 }
